8e3d8afae9351a959761f6a6d7516b06791ce084
binu-baiju <binubaiju000@gmail.com>
2022-12-12 22:38:43 +0530

    edited mathoperations.jest.js

--- Week-5/mathoperations.test.js
+++ Week-5/mathoperations.test.js
@@ -6,43 +6,43 @@ describe("MathOperations",()=>{
         var result = mathOperations.sum(1,2);
         expect(result).toBe(3)
     })
-    test("sum :-adding 1 & 2 return 10 will be incorrect",()=>{
+    test("sum :-adding -1 & 2 return 1",()=>{
         var result = mathOperations.sum(-1,2);
         expect(result).toBe(1)
     })
-    test("sum :-adding 1 & 2 return 3 ",()=>{
+    test("sum :-adding 1 & -2 return -1 ",()=>{
         var result = mathOperations.sum(1,-2);
         expect(result).toBe(-1)
     })
-    test("sum :-adding 1 & 2 return 3 ",()=>{
+    test("sum :-adding -1 & -2 return -3 ",()=>{
         var result = mathOperations.sum(-1,-2);
         expect(result).toBe(-3)
     })
-    test("diff :-substracting 1 from 2 return 3 ",()=>{
+    test("diff :-substracting 1 from 2 return -1 ",()=>{
         var result = mathOperations.diff(1,2);
         expect(result).toBe(-1)
     })
-    test("diff :-substracting 3 from 1 return 3 ",()=>{
+    test("diff :-substracting -1 from 2 return -3 ",()=>{
         var result = mathOperations.diff(-1,2);
         expect(result).toBe(-3)
     })
-    test("diff :-substracting 3 from 1 return 3 ",()=>{
+    test("diff :-substracting 1 from -2 return 3 ",()=>{
         var result = mathOperations.diff(1,-2);
         expect(result).toBe(3)
     })
-    test("diff :-substracting 3 from 1 return 3 ",()=>{
+    test("diff :-substracting -1 from -2 return 1 ",()=>{
         var result = mathOperations.diff(-1,-2);
         expect(result).toBe(1)
     })
-    test("product :-substracting 3 from 1 return 3 ",()=>{
+    test("product :-multiplying 1 and 2 return 2 ",()=>{
         var result = mathOperations.product(1,2);
         expect(result).toBe(2)
     })
-    test("product :-substracting 3 from 1 return 3 ",()=>{
+    test("product :-multiplying -1 and 2 return -2 ",()=>{
         var result = mathOperations.product(-1,2);
         expect(result).toBe(-2)
     }) 
-    test("product :-substracting 3 from 1 return 3 ",()=>{
+    test("product :-multiplying -1 and -2 return 2 ",()=>{
         var result = mathOperations.product(-1,-2);
         expect(result).toBe(2)
     })   
