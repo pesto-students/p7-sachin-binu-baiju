741de8960e7990307cc07328a6f84d0fe49bf6e5
binu-baiju <binubaiju000@gmail.com>
2023-03-16 22:31:31 +0530

    Assignmetn done

--- Week-13/Weather App/app.js
+++ Week-13/Weather App/app.js
@@ -3,11 +3,11 @@ const axios = require("axios");
 
 const server = http.createServer(function (req, res) {
   const API_KEY = "8fde9908dbbd67683ee47c18673c3af2";
- const WEATHER_API="https://api.openweathermap.org/data/2.5/weather";
+  const WEATHER_API = "https://api.openweathermap.org/data/2.5/weather";
   //API call to get weather data by city name
   if (req.url === "/citybycityname") {
     city = "london";
-    let url =`${WEATHER_API}?q=${city}&appid= ${API_KEY}`;
+    let url = `${WEATHER_API}?q=${city}&appid=${API_KEY}`;
     axios.get(url).then((response) => {
       let resarr = [];
       resarr.push({
@@ -28,7 +28,7 @@ const server = http.createServer(function (req, res) {
   //API call to get weather data by city Id
   if (req.url === "/citybycityId") {
     const cityId = 2643743;
-    let url =`${WEATHER_API}?q=${cityId}&appid=${API_KEY}`;
+    let url = `${WEATHER_API}?id=${cityId}&appid=${API_KEY}`;
     axios.get(url).then((response) => {
       let resarr = [];
       resarr.push({
@@ -48,9 +48,7 @@ const server = http.createServer(function (req, res) {
   }
   //API call to get weather data by city Id
   if (req.url === "/all") {
-    const cityId = 2643743;
-    let url =
-      `http://api.openweathermap.org/data/2.5/group?id=2643741,2644688,2633352,2654675,2988507,
+    let url = `http://api.openweathermap.org/data/2.5/group?id=2643741,2644688,2633352,2654675,2988507,
       2990969,2911298,2925535,2950159,3120501,3128760,5128581,4140963,4930956,5106834,5391959,
       5368361,5809844,4099974,4440906&appid=${API_KEY}&units=metric`;
     axios.get(url).then((response) => {
@@ -87,9 +85,8 @@ const server = http.createServer(function (req, res) {
   }
 
   const FORECAST_URL = "http://api.openweathermap.org/data/2.5/forecast";
-
+  // Detailed Forecast for the next X days
   if (req.url === "/forecast") {
-
     getForecast(7); // Get the 7-day forecast
 
     function getForecast(numDays) {
@@ -99,14 +96,16 @@ const server = http.createServer(function (req, res) {
         .then((response) => {
           let resarr = [];
           const forecast = response.data.list.slice(0, numDays);
-
+          resarr.push(`Forecast for the next ${numDays} days of London`);
           forecast.forEach((day) => {
             const date = new Date(day.dt * 1000);
             const temp = day.main.temp;
             const conditions = day.weather[0].description;
             const correctdate = date.toDateString();
 
-            resarr.push(correctdate, temp, conditions);
+            resarr.push(
+              `correctdate:${correctdate},temp:${temp}°C,condition:${conditions}`
+            );
           });
           let responsestr = JSON.stringify(resarr);
           res.setHeader("Content-Type", "application/json");
@@ -119,6 +118,7 @@ const server = http.createServer(function (req, res) {
         });
     }
   }
+  // Filter the data by any particular city, any particular date
   if (req.url === "/forecastbydatecity") {
     getForecast(10, "2023-03-20", "London"); // Get the 10-day forecast starting from March 20, 2023 of london
     function getForecast(numDays, startDate, city) {
@@ -135,9 +135,11 @@ const server = http.createServer(function (req, res) {
               date < new Date(start.getTime() + numDays * 24 * 60 * 60 * 1000)
             );
           });
-          console.log(
-            `Forecast for the next ${numDays} days starting from ${startDate}:`
+
+          resarr.push(
+            `Forecast for the next ${numDays} days starting from ${startDate}:of ${city}`
           );
+
           forecast.forEach((day) => {
             const date = new Date(day.dt * 1000);
             const temp = day.main.temp;
