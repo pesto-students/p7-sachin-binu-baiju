45b6fdf5a0d4ea32ad749f7ffb8dc056982f5874
binu-baiju <binubaiju000@gmail.com>
2023-03-16 22:20:09 +0530

    Forecast by city and date done

--- Week-13/Weather App/app.js
+++ Week-13/Weather App/app.js
@@ -2,48 +2,12 @@ const http = require("http");
 const axios = require("axios");
 
 const server = http.createServer(function (req, res) {
-  //   let urls = [
-  //     `http://api.openweathermap.
-  // 	org/data/2.5/weather?q=ernakulam&units=metric&appid=8fde9908dbbd67683ee47c18673c3af2`,
-  //     `http://api.openweathermap.
-  // 		org/data/2.5/weather?q=thrissur&units=metric&appid=8fde9908dbbd67683ee47c18673c3af2`,
-  //   ];
-  //   if (req.url === "/multiplecities" && req.method === "GET") {
-  //     const requests = urls.map((url) => axios.get(url));
-  //     Promise.all(requests)
-  //       .then((responses) => {
-  //         let responsearr = [];
-
-  //         responses.forEach((response) => {
-  //           responsearr.push({
-  //             City: response.data.name,
-  //             WeatherDetails: {
-  //               main: response.data.weather[0].main,
-  //               description: response.data.weather[0].description,
-  //               icon: response.data.weather[0].icon,
-  //             },
-  //           });
-  //           console.log(response.data);
-
-  //           //console.log(response.data.weather);
-  //         });
-  //         let responsearrstr = JSON.stringify(responsearr);
-  //         res.setHeader("Content-Type", "application/json");
-  //         res.writeHead(200);
-  //         res.write(responsearrstr);
-  //         res.end();
-  //       })
-  //       .catch((error) => {
-  //         console.log(error);
-  //       });
-  //   }
+  const API_KEY = "8fde9908dbbd67683ee47c18673c3af2";
+ const WEATHER_API="https://api.openweathermap.org/data/2.5/weather";
   //API call to get weather data by city name
   if (req.url === "/citybycityname") {
     city = "london";
-    let url =
-      "https://api.openweathermap.org/data/2.5/weather?q=" +
-      city +
-      "&appid=8fde9908dbbd67683ee47c18673c3af2";
+    let url =`${WEATHER_API}?q=${city}&appid= ${API_KEY}`;
     axios.get(url).then((response) => {
       let resarr = [];
       resarr.push({
@@ -64,10 +28,7 @@ const server = http.createServer(function (req, res) {
   //API call to get weather data by city Id
   if (req.url === "/citybycityId") {
     const cityId = 2643743;
-    let url =
-      "https://api.openweathermap.org/data/2.5/weather?id=" +
-      cityId +
-      "&appid=8fde9908dbbd67683ee47c18673c3af2";
+    let url =`${WEATHER_API}?q=${cityId}&appid=${API_KEY}`;
     axios.get(url).then((response) => {
       let resarr = [];
       resarr.push({
@@ -89,7 +50,9 @@ const server = http.createServer(function (req, res) {
   if (req.url === "/all") {
     const cityId = 2643743;
     let url =
-      "http://api.openweathermap.org/data/2.5/group?id=2643741,2644688,2633352,2654675,2988507,2990969,2911298,2925535,2950159,3120501,3128760,5128581,4140963,4930956,5106834,5391959,5368361,5809844,4099974,4440906&appid=de6d52c2ebb7b1398526329875a49c57&units=metric";
+      `http://api.openweathermap.org/data/2.5/group?id=2643741,2644688,2633352,2654675,2988507,
+      2990969,2911298,2925535,2950159,3120501,3128760,5128581,4140963,4930956,5106834,5391959,
+      5368361,5809844,4099974,4440906&appid=${API_KEY}&units=metric`;
     axios.get(url).then((response) => {
       let responsestr = JSON.stringify(response.data);
       res.setHeader("Content-Type", "application/json");
@@ -105,71 +68,96 @@ const server = http.createServer(function (req, res) {
 
   // Make a request to the WeatherAPI with the pagination parameters
   if (req.url === "/pagination") {
-  axios
-    .get("http://api.openweathermap.org/data/2.5/weather", {
-      params: {
-        q: "London",
-        appid: "8fde9908dbbd67683ee47c18673c3af2",
-        cnt: pageSize,
-        page: pageNum,
-      },
-    })
-    .then((response) => {
-      let responsestr = JSON.stringify(response.data);
-      res.setHeader("Content-Type", "application/json");
-      res.writeHead(200);
-      res.write(responsestr);
-      res.end();
-
-    });
-  
-
+    axios
+      .get("http://api.openweathermap.org/data/2.5/weather", {
+        params: {
+          q: "London",
+          appid: API_KEY,
+          cnt: pageSize,
+          page: pageNum,
+        },
+      })
+      .then((response) => {
+        let responsestr = JSON.stringify(response.data);
+        res.setHeader("Content-Type", "application/json");
+        res.writeHead(200);
+        res.write(responsestr);
+        res.end();
+      });
   }
 
-  // const API_KEY = 'de6d52c2ebb7b1398526329875a49c57';
-  // const CITY = 'london';
-  // const PAGE_SIZE = 10;
+  const FORECAST_URL = "http://api.openweathermap.org/data/2.5/forecast";
 
-  // async function getWeatherData(page) {
-  //   const startIndex = (page - 1) * PAGE_SIZE;
-  //   const endIndex = startIndex + PAGE_SIZE - 1;
-  //   const url = `https://api.openweathermap.org/data/2.5/forecast?q=${CITY}&appid=${API_KEY}`;
+  if (req.url === "/forecast") {
 
-  //   try {
-  //     const response = await axios.get(url);
-  //     const weatherData = response.data.list.slice(startIndex, endIndex + 1);
-  //     return weatherData;
-  //   } catch (error) {
-  //     console.log(error);
-  //   }
-  // }
+    getForecast(7); // Get the 7-day forecast
 
-  // // Usage example
-  // if (req.url === "/pagination") {
-  // getWeatherData(1) // Retrieves first page of data (items 0 to 9)
-  //   .then((data) => {
-  //     let responsestr = JSON.stringify(data);
-  //     res.setHeader("Content-Type", "application/json");
-  //     res.writeHead(200);
-  //     res.write(responsestr);
-  //     res.end();
+    function getForecast(numDays) {
+      const url = `${FORECAST_URL}?q=London&appid=${API_KEY}&units=metric`;
+      axios
+        .get(url)
+        .then((response) => {
+          let resarr = [];
+          const forecast = response.data.list.slice(0, numDays);
 
-  //   })
-  //   .catch(error => console.log(error));
-  // }
-  // if (req.url === "/pagination2") {
+          forecast.forEach((day) => {
+            const date = new Date(day.dt * 1000);
+            const temp = day.main.temp;
+            const conditions = day.weather[0].description;
+            const correctdate = date.toDateString();
 
-  // getWeatherData(2) // Retrieves second page of data (items 10 to 19)
-  // .then((data) => {
-  //   let responsestr = JSON.stringify(data);
-  //   res.setHeader("Content-Type", "application/json");
-  //   res.writeHead(200);
-  //   res.write(responsestr);
-  //   res.end();
-
-  // })
-  // .catch(error => console.log(error));
-
-  // }
+            resarr.push(correctdate, temp, conditions);
+          });
+          let responsestr = JSON.stringify(resarr);
+          res.setHeader("Content-Type", "application/json");
+          res.writeHead(200);
+          res.write(responsestr);
+          res.end();
+        })
+        .catch((error) => {
+          console.error(error);
+        });
+    }
+  }
+  if (req.url === "/forecastbydatecity") {
+    getForecast(10, "2023-03-20", "London"); // Get the 10-day forecast starting from March 20, 2023 of london
+    function getForecast(numDays, startDate, city) {
+      const url = `${FORECAST_URL}?q=${city}&appid=${API_KEY}&units=metric`;
+      axios
+        .get(url)
+        .then((response) => {
+          let resarr = [];
+          const forecast = response.data.list.filter((day) => {
+            const date = new Date(day.dt * 1000);
+            const start = new Date(startDate);
+            return (
+              date >= start &&
+              date < new Date(start.getTime() + numDays * 24 * 60 * 60 * 1000)
+            );
+          });
+          console.log(
+            `Forecast for the next ${numDays} days starting from ${startDate}:`
+          );
+          forecast.forEach((day) => {
+            const date = new Date(day.dt * 1000);
+            const temp = day.main.temp;
+            const conditions = day.weather[0].description;
+            const correctdate = date.toDateString();
+            resarr.push(
+              `correctdate:${correctdate},temp:${temp}°C,condition:${conditions}`
+            );
+            console.log(`${date.toDateString()}: ${temp}°C, ${conditions}`);
+          });
+          let responsestr = JSON.stringify(resarr);
+          res.setHeader("Content-Type", "application/json");
+          res.writeHead(200);
+          res.write(responsestr);
+          res.end();
+        })
+        .catch((error) => {
+          console.error(error);
+        });
+    }
+  }
 });
 server.listen(7000);
